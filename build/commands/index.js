0968ce2f72af523d0fbdb45c31f6405114f02684
Joe Johans <lgvichy@gmail.com>
2019-06-06 18:10:12 -0700

    FEAT | piche now setup tmp or home localtunnel

--- build/commands/index.js
+++ build/commands/index.js
@@ -125,28 +125,176 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = void 0;
 
-var _react = _interopRequireDefault(require("react"));
-
-var _propTypes = _interopRequireDefault(require("prop-types"));
-
-var _ink = require("ink");
+var _os = _interopRequireDefault(require("os"));
 
 var _http = _interopRequireDefault(require("http"));
 
 var _serveHandler = _interopRequireDefault(require("serve-handler"));
 
+var _localtunnel = _interopRequireDefault(require("localtunnel"));
+
+var _clipboardy = _interopRequireDefault(require("clipboardy"));
+
+var _fsExtra = _interopRequireDefault(require("fs-extra"));
+
+var _react = _interopRequireWildcard(require("react"));
+
+var _propTypes = _interopRequireDefault(require("prop-types"));
+
+var _ink = require("ink");
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = Object.defineProperty && Object.getOwnPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : {}; if (desc.get || desc.set) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } } newObj.default = obj; return newObj; } }
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-/// Hello world command
-const Piche = ({
-  name
+const useLogState = (tag, defaultValue = "hello", defaultColor = "white") => {
+  const [log, setLogRaw] = (0, _react.useState)(`${tag}\t | ${defaultValue}`);
+
+  const setLog = s => setLogRaw(`${tag}\t | ${s}`);
+
+  const [color, setColor] = (0, _react.useState)(defaultColor);
+  return [log, setLog, color, setColor];
+};
+
+const PicheStart = ({
+  tmp
 }) => {
-  return _react.default.createElement(_ink.Text, null, "Hello, ", name);
+  const workingPath = `${tmp ? _os.default.tmpdir() : _os.default.homedir()}/.piche`;
+  const publicPath = `${workingPath}/public`;
+  const statusFilePath = `${workingPath}/status.json`;
+  const [localStatus, setLocalStatus, localStatusColor, setLocalStatusColor] = useLogState("piche-local", "setup server . . .", "yellow");
+  const [tunnelStatus, setTunnelStatus, tunnelStatusColor, setTunnelStatusColor] = useLogState("piche-tunnel", "waiting for piche-local . . .", "orange");
+  const [tunnel, setTunnel] = (0, _react.useState)();
+  (0, _react.useEffect)(() => {
+    _fsExtra.default.ensureDirSync(publicPath);
+
+    const server = new _http.default.Server((req, res) => {
+      return (0, _serveHandler.default)(req, res, {
+        public: publicPath
+      });
+    });
+    server.listen(0, () => {
+      const localtunnelPort = server.address().port;
+      setLocalStatusColor("pink");
+      setTunnelStatusColor("orange");
+      setLocalStatus(`up and running at http://localhost:${localtunnelPort}`);
+      setTunnelStatus(`setting up tunnel to ${localtunnelPort}`);
+      (0, _localtunnel.default)(localtunnelPort, async (err, tunnel) => {
+        if (err) {
+          setTunnelStatusColor("red");
+          setLocalStatus(err);
+          server.close();
+        }
+
+        setTunnelStatusColor("green");
+        setTunnelStatus(`up and running at ${tunnel.url}`);
+        await _fsExtra.default.outputJson(statusFilePath, {
+          port: localtunnelPort,
+          url: tunnel.url
+        });
+        setTunnel(tunnel);
+      });
+    });
+    process.on("SIGINT", () => {
+      setLocalStatusColor("magenta");
+      setTunnelStatusColor("cyan");
+      setLocalStatus("shutdown.");
+      setTunnelStatus("shutdown.");
+
+      _fsExtra.default.removeSync(statusFilePath);
+
+      if (tmp) {
+        _fsExtra.default.removeSync(workingPath);
+      }
+
+      if (tunnel) {
+        tunnel.close();
+      }
+
+      process.exit();
+    });
+  }, []);
+  return _react.default.createElement(_ink.Box, {
+    flexDirection: "column"
+  }, _react.default.createElement(_ink.Color, {
+    keyword: localStatusColor
+  }, _react.default.createElement(_ink.Text, null, localStatus)), _react.default.createElement(_ink.Color, {
+    keyword: tunnelStatusColor
+  }, _react.default.createElement(_ink.Text, null, tunnelStatus)));
+};
+
+const PicheClean = () => {
+  const tmpPath = `${_os.default.tmpdir()}/.piche`;
+  const homePath = `${_os.default.homedir()}/.piche`;
+  const [status, setStatus, statusColor, setStatusColor] = useLogState("piche-clean", "warming up . . .", "yellow");
+  (0, _react.useEffect)(() => {
+    const cleanup = async () => {
+      setStatus(`clean up ${tmpPath} . . .`);
+      await _fsExtra.default.remove(tmpPath);
+      setStatusColor("orange");
+      setStatus(`clean up ${homePath} . . .`);
+      await _fsExtra.default.remove(homePath);
+      setStatusColor("green");
+      setStatus("done.");
+    };
+
+    cleanup();
+  }, []);
+  return _react.default.createElement(_ink.Color, {
+    keyword: statusColor
+  }, _react.default.createElement(_ink.Text, null, status));
+}; /// piche a piece of text and send it to the temp folder
+
+
+const Piche = ({
+  start,
+  tmp,
+  clean
+}) => {
+  const [data, setData] = (0, _react.useState)("loading . . .");
+
+  if (clean) {
+    return _react.default.createElement(PicheClean, null);
+  }
+
+  if (start) {
+    return _react.default.createElement(PicheStart, {
+      tmp: tmp
+    });
+  }
+
+  const workingPath = `${tmp ? _os.default.tmpdir() : _os.default.homedir()}/.piche`;
+  const publicPath = `${workingPath}/public`;
+  const statusFilePath = `${workingPath}/status.json`;
+  (0, _react.useEffect)(() => {
+    const timer = setTimeout(() => {
+      process.stdin.destroy();
+    }, 1000);
+    process.stdin.once("data", e => {
+      clearTimeout(timer);
+      setData(e.toString());
+    });
+  }, []);
+  return _react.default.createElement(_ink.Text, null, "Here comes the data: ", data);
 };
 
 Piche.propTypes = {
-  /// Name of the person to greet
-  name: _propTypes.default.string.isRequired
+  /// Start piche server
+  start: _propTypes.default.bool,
+  /// Use os.tmpdir/.piche instead of os.homedir/.piche
+  tmp: _propTypes.default.bool,
+  /// Cleanup os.tmpdir/.piche and os.homedir/.piche
+  clean: _propTypes.default.bool
+};
+Piche.defaultProps = {
+  start: false,
+  tmp: false,
+  clean: false
+};
+Piche.shortFlags = {
+  start: "s",
+  tmp: "t",
+  clean: "c"
 };
 var _default = Piche;
 exports.default = _default;
--- commands/index.js
+++ commands/index.js
@@ -1,24 +1,203 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import {Text} from 'ink';
+import os from "os";
+import http from "http";
+import handler from "serve-handler";
+import localtunnel from "localtunnel";
+import clipboard from "clipboardy";
+import fs from "fs-extra";
 
-import http from 'http'
-import handler from 'serve-handler'
+import React, { useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import { Text, Color, Box } from "ink";
 
+const useLogState = (tag, defaultValue = "hello", defaultColor = "white") => {
+	const [log, setLogRaw] = useState(`${tag}\t | ${defaultValue}`);
 
-/// Hello world command
-const Piche = ({name}) => {
+	const setLog = s => setLogRaw(`${tag}\t | ${s}`);
 
+	const [color, setColor] = useState(defaultColor);
 
+	return [log, setLog, color, setColor];
+};
+
+const PicheStart = ({ tmp }) => {
+	const workingPath = `${tmp ? os.tmpdir() : os.homedir()}/.piche`;
+
+	const publicPath = `${workingPath}/public`;
+
+	const statusFilePath = `${workingPath}/status.json`;
+
+	const [
+		localStatus,
+		setLocalStatus,
+		localStatusColor,
+		setLocalStatusColor
+	] = useLogState("piche-local", "setup server . . .", "yellow");
+
+	const [
+		tunnelStatus,
+		setTunnelStatus,
+		tunnelStatusColor,
+		setTunnelStatusColor
+	] = useLogState("piche-tunnel", "waiting for piche-local . . .", "orange");
+
+	const [tunnel, setTunnel] = useState();
+
+	useEffect(() => {
+		fs.ensureDirSync(publicPath);
+
+		const server = new http.Server((req, res) => {
+			return handler(req, res, {
+				public: publicPath
+			});
+		});
+
+		server.listen(0, () => {
+			const localtunnelPort = server.address().port;
+
+			setLocalStatusColor("pink");
+			setTunnelStatusColor("orange");
+
+			setLocalStatus(`up and running at http://localhost:${localtunnelPort}`);
+
+			setTunnelStatus(`setting up tunnel to ${localtunnelPort}`);
+
+			localtunnel(localtunnelPort, async (err, tunnel) => {
+				if (err) {
+					setTunnelStatusColor("red");
+					setLocalStatus(err);
+					server.close();
+				}
+				setTunnelStatusColor("green");
+				setTunnelStatus(`up and running at ${tunnel.url}`);
+
+				await fs.outputJson(statusFilePath, {
+					port: localtunnelPort,
+					url: tunnel.url
+				});
+
+				setTunnel(tunnel);
+			});
+		});
+
+		process.on("SIGINT", () => {
+			setLocalStatusColor("magenta");
+			setTunnelStatusColor("cyan");
+			setLocalStatus("shutdown.");
+			setTunnelStatus("shutdown.");
+
+			fs.removeSync(statusFilePath);
+
+			if (tmp) {
+				fs.removeSync(workingPath);
+			}
+
+			if (tunnel) {
+				tunnel.close();
+			}
+
+			process.exit();
+		});
+	}, []);
 
 	return (
-		<Text>Hello, {name}</Text>
+		<Box flexDirection="column">
+			<Color keyword={localStatusColor}>
+				<Text>{localStatus}</Text>
+			</Color>
+			<Color keyword={tunnelStatusColor}>
+				<Text>{tunnelStatus}</Text>
+			</Color>
+		</Box>
 	);
-}
+};
+
+const PicheClean = () => {
+	const tmpPath = `${os.tmpdir()}/.piche`;
+	const homePath = `${os.homedir()}/.piche`;
+
+	const [status, setStatus, statusColor, setStatusColor] = useLogState(
+		"piche-clean",
+		"warming up . . .",
+		"yellow"
+	);
+	useEffect(() => {
+		const cleanup = async () => {
+			setStatus(`clean up ${tmpPath} . . .`);
+
+			await fs.remove(tmpPath);
+
+			setStatusColor("orange");
+			setStatus(`clean up ${homePath} . . .`);
+
+			await fs.remove(homePath);
+
+			setStatusColor("green");
+			setStatus("done.");
+		};
+
+		cleanup();
+	}, []);
+
+	return (
+		<Color keyword={statusColor}>
+			<Text>{status}</Text>
+		</Color>
+	);
+};
+
+/// piche a piece of text and send it to the temp folder
+const Piche = ({ start, tmp, clean }) => {
+	const [data, setData] = useState("loading . . .");
+
+	if (clean) {
+		return <PicheClean />;
+	}
+
+	if (start) {
+		return <PicheStart tmp={tmp} />;
+	}
+
+	const workingPath = `${tmp ? os.tmpdir() : os.homedir()}/.piche`;
+
+	const publicPath = `${workingPath}/public`;
+
+	const statusFilePath = `${workingPath}/status.json`;
+
+
+	useEffect(() => {
+		const timer = setTimeout(() => {
+			process.stdin.destroy();
+		}, 1000);
+
+		process.stdin.once("data", e => {
+			clearTimeout(timer);
+
+			setData(e.toString());
+		});
+	}, []);
+
+	return <Text>Here comes the data: {data}</Text>;
+};
 
 Piche.propTypes = {
-	/// Name of the person to greet
-	name: PropTypes.string.isRequired
+	/// Start piche server
+	start: PropTypes.bool,
+	/// Use os.tmpdir/.piche instead of os.homedir/.piche
+	tmp: PropTypes.bool,
+	/// Cleanup os.tmpdir/.piche and os.homedir/.piche
+	clean: PropTypes.bool
+};
+
+Piche.defaultProps = {
+	start: false,
+	tmp: false,
+	clean: false
+};
+
+Piche.shortFlags = {
+	start: "s",
+	tmp: "t",
+	clean: "c"
 };
 
 export default Piche;
